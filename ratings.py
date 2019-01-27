16831087944de10b580e4ca28e5507b3830d15c9
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-26 16:14:39 -0800

    ratings.py

--- ratings.py
+++ ratings.py
@@ -1,4 +1,26 @@
 """Restaurant rating lister."""
 
+def get_ratings(filename):
 
-# put your code here
+    ratings = {}
+
+    file = open(filename)
+
+    for line in file:
+        line = line.rstrip()
+        line_data = line.split(":")
+
+        #if ratings[line_data[0]] not in ratings:
+        ratings[line_data[0]] = line_data[1]
+
+    file.close()
+
+    return ratings
+
+
+ratings_dict = get_ratings('scores.txt')
+
+items = sorted(list(ratings_dict.items()))
+
+for item in items:
+    print('{} is rated at {}.'.format(item[0], item[1])) 
